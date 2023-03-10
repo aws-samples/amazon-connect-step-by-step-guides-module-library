f26e1bf3d4c50fbd7f77018e111932b7af52c6ed
David Kocen <kocedavi@amazon.com>
2023-03-10 11:18:12 -0800

    I had copied the wrong code after finalizing. Adjusting

--- examples/BackendIntegration/sampleLambdas/sendSMS.js
+++ examples/BackendIntegration/sampleLambdas/sendSMS.js
@@ -1,58 +1,37 @@
-import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+import { 
+  SNSClient, 
+  SetSMSAttributesCommand, 
+  PublishCommand 
+} from "@aws-sdk/client-sns";
 
-export const handler = async (event, context, callback) => {
 
-    const sesClient = new SESClient();
-    
-    const dataFromForm = event.Details.Parameters.formData;    
-    const customerName = event.Details.Parameters.customerFullName;
-    console.log(dataFromForm);
-
-    const messageBodyHTML = 
-        `<p>Message: ${customerName}</p>
-        
-        <h4>Message from SBSGuides</h4>
-        <ul>
-          <li>Date: ${dataFromForm['appt-day']}</li>
-          <li>Time: ${dataFromForm['appt-time']}</li>
-          <li>Details: ${dataFromForm['message-details']}</li>
-        </ul>`;
-        
-    const messageBody = `Message from SBSGuides. ${customerName}, with data 'date': ${dataFromForm['appt-day']}, 'time': ${dataFromForm['appt-time']}, and 'details':${dataFromForm['message-details']}`;
-    
-    const sendEmailCommand = new SendEmailCommand({
-      Destination: { /* required */
-        ToAddresses: [
-          event.Details.Parameters.destinationEmail,
-        ]
-      },
-      Message: { /* required */
-        Body: { /* required */
-          Html: {
-          Charset: "UTF-8",
-          Data: messageBodyHTML
-          },
-          Text: {
-          Charset: "UTF-8",
-          Data: messageBody
-          }
-        },
-        Subject: {
-          Charset: 'UTF-8',
-          Data: 'Message from SBSGuides'
-        }
-        },
-      Source: event.Details.Parameters.systemEmail
-    });
-    
-    try {
-      const response = await sesClient.send(sendEmailCommand);
-      return {"status": 200}
-      
-    } catch (e) {
-      
-      console.error("Failed to send email.");
+export const handler = async(event) => {
+  
+  const snsClient = new SNSClient();
+  console.log(event)
+  
+  const dataFromForm = event.Details.Parameters.formData;
+  const destination = event.Details.Parameters.destination;
+  const customerName = event.Details.Parameters.customerFullName;
+  console.log(dataFromForm);
+  
+  const params = {
+      PhoneNumber: destination,
+      Message: `Message from SBSGuides. ${customerName}, with data 'date': ${dataFromForm['appt-day']}, 'time': ${dataFromForm['appt-time']}, and 'details':${dataFromForm['message-details']}`
+  }
+  
+  const setAttributesCommand = new SetSMSAttributesCommand({
+      attributes: {
+          DefaultSMSType: "Transactional"
+      }
+  });
+  
+  
+  try {
+      await snsClient.send(setAttributesCommand);
+      return await snsClient.send(new PublishCommand(params));
+  } catch (e) {
       console.log(e);
-      return {"status": 500}
-    }
+      return e;
+  }
 };
